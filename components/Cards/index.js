3bbb0123af1a3930a18e2e18d44ee47de04c4194
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2019-10-04 10:08:10 -0700

    completed cards component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,47 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function createCard(data) {
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const imgAuthor = document.createElement('img');
+    const authorName = document.createElement('span');
+
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+   imgAuthor.src = data.authorPhoto;
+   headline.textContent = data.headline;
+   authorName.textContent = data.authorName;
+
+   card.appendChild(headline);
+   card.appendChild(author);
+
+   author.appendChild(imgContainer);
+   author.appendChild(authorName);
+   imgContainer.appendChild(imgAuthor);
+
+   const cardsContainer = document.querySelector('.cards-container');
+   cardsContainer.append(card);
+
+   return card;
+
+}
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles') 
+    .then((res) => { 
+        console.log(res);
+        res.data.articles.javascript.forEach(article => createCard(article));
+        res.data.articles.bootstrap.forEach(article => createCard(article));
+        res.data.articles.jquery.forEach(article => createCard(article));
+        res.data.articles.node.forEach(article => createCard(article));
+        res.data.articles.technology.forEach(article => createCard(article));
+    })
+    .catch((error) => { 
+        console.log(error) 
+    })
